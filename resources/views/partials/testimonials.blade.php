a067d9f597e078eb44b097b9a34b92cb114eb153
ggeorgiev6 <gecatalost@gmail.com>
2022-05-26 23:59:53 +0100

    Testimonials tweaks

--- resources/views/partials/testimonials.blade.php
+++ resources/views/partials/testimonials.blade.php
@@ -1,4 +1,4 @@
-<div class="testimonialsSection">
+<section class="testimonialsSection">
     <div class="container">
         <div class="row">
             <div class="col-12 col-lg-6">
@@ -8,38 +8,46 @@
                     <div class="swiper-wrapper">
                     
                         <div class="swiper-slide" >
-                            
-                            <div class="testimonialsInnerText h2 text-secondary">
-                                You have taken the time to understand us, our values
-                                and culture and have delivered far more than we could have hoped for.
-                            </div>
 
-                            <div class="testimonialsAuthor h3 text-secondary">
-                                Ben Olson, Managing Director
-                                Grosvenor Consultancy
-                            </div>
+                            <div class="testimonialsInnerWrap">
 
-                            <div class="testimonialsCompany text-primary">
-                                Growth Academy
+                                    <div class="testimonialsInnerText h2 text-secondary">
+                                        You have taken the time to understand us, our values
+                                        and culture and have delivered far more than we could have hoped for.
+                                    </div>
+
+                                    <div class="testimonialsAuthor h3 text-secondary">
+                                        Ben Olson, Managing Director
+                                        Grosvenor Consultancy
+                                    </div>
+
+                                    <div class="testimonialsCompany text-primary">
+                                        Growth Academy
+                                    </div>
                             </div>
                         
                         </div>
 
                         <div class="swiper-slide" >
-                            
-                            <div class="testimonialsInnerText h2 text-secondary">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-                            </div>
 
-                            <div class="testimonialsAuthor h3 text-secondary">
-                                Ben Olson, Managing Director
-                                Grosvenor Consultancy
-                            </div>
+                            <div class="testimonialsInnerWrap">
 
-                            <div class="testimonialsCompany text-primary">
-                                Growth Academy
+                                <div class="testimonialsInnerText h2 text-secondary">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+                                </div>
+
+                                <div class="testimonialsAuthor h3 text-secondary">
+                                    Ben Olson, Managing Director
+                                    Grosvenor Consultancy
+                                </div>
+
+                                <div class="testimonialsCompany text-primary">
+                                    Growth Academy
+                                </div>
+
                             </div>
+                           
                         
                         </div>
                     
@@ -59,4 +67,4 @@
     <div class="testimonialImg">
         <img src="/images/testimonial-puzzle.png" alt="">
     </div>
-</div>
+</section>
