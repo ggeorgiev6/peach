563068415bea4f09a2078f4d48118b5f1f991eda
ggeorgiev6 <gecatalost@gmail.com>
2022-05-27 00:00:44 +0100

    Partners partial added

---
+++ resources/views/partials/partners.blade.php
@@ -0,0 +1,34 @@
+<section class="partnersSection">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="partnersSliderWrap text-center ">
+                    <h4 class="partnersSliderHeading h3 text-primary">People we work with</h4>
+                    <div class="partnersSlider swiper">
+                        <div class="swiper-wrapper">
+                            <div class="swiper-slide">
+                                <img src="/images/accreds-1.svg" alt="">
+                            </div>
+                            <div class="swiper-slide">
+                                <img src="/images/accreds-2.svg" alt="">
+                            </div>
+                            <div class="swiper-slide">
+                                <img src="/images/accreds-3.svg" alt="">
+                            </div>
+                            <div class="swiper-slide">
+                                <img src="/images/accreds-4.svg" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="partnersControlsWrap">
+                        <div class="slider-pagination partners-pagination-slider"></div>
+                    </div>
+                    <div class="partnersNavWrap">
+                        <div class="partners-slider-button-prev partnersSliderArrow"></div>
+                        <div class="partners-slider-button-next partnersSliderArrow"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
