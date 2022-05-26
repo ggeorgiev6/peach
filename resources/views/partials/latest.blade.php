0954858bf1ddccf0d5fdb37060721896bfbd3e75
ggeorgiev6 <gecatalost@gmail.com>
2022-05-27 00:01:17 +0100

    Latest articles partial

---
+++ resources/views/partials/latest.blade.php
@@ -0,0 +1,86 @@
+<section class="latestNewsSection">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-12">
+                <div class="latestNewsHeadWrap">
+                    <h5 class="text-secondary latestNewsHeading">
+                        The <u>latest  </u>
+                    </h5>
+                    <a class="textButton" href="">
+                        All News
+                    </a>
+                </div>
+            </div>
+            
+        </div>
+
+        <div class="row cardsRow">
+
+            <div class="col-12 col-lg-4 cardCol">
+
+                <div class="card">
+                    <a href="">
+                        <div class="outerCardShape"></div>
+                        <div class="innerCardShape text-center">
+                            <div class="innerCardBg">
+                                <img class="innerCardImg" src="/images/news_image_2.jpg" alt="">
+                            </div>
+                            <div class="innerCardText fw-semi-bold">
+                                <div class="cardCategory text-secondary h3 ">Resources</div>
+                                <div class="cardTitle text-white h2 ">Download the Three Pillars of Successful Selling e-Book</div>
+                                <div class="cardDate h4 text-white fw-semi-bold">15.08.19</div>
+                            </div>
+                        </div>
+                    </a>
+                    
+                </div>
+
+            </div>
+
+            <div class="col-12 col-lg-4 cardCol">
+
+                <div class="card">
+                    <a href="">
+                        <div class="outerCardShape"></div>
+                        <div class="innerCardShape text-center">
+                            <div class="innerCardBg">
+                                <img class="innerCardImg" src="/images/news_image_1.jpg" alt="">
+                            </div>
+                            <div class="innerCardText fw-semi-bold">
+                                <div class="cardCategory text-secondary h3 ">Resources</div>
+                                <div class="cardTitle text-white h2 ">Download the Three Pillars of Successful Selling e-Book</div>
+                                <div class="cardDate h4 text-white fw-semi-bold">15.08.19</div>
+                            </div>
+                        </div>
+                    </a>
+                    
+                </div>
+
+            </div>
+
+            <div class="col-12 col-lg-4 cardCol">
+
+                <div class="card">
+                    <a href="">
+                        <div class="outerCardShape"></div>
+                        <div class="innerCardShape text-center">
+                            <div class="innerCardBg">
+                                <img class="innerCardImg" src="/images/news_image_2.jpg" alt="">
+                            </div>
+                            <div class="innerCardText fw-semi-bold">
+                                <div class="cardCategory text-secondary h3 ">Resources</div>
+                                <div class="cardTitle text-white h2 ">Download the Three Pillars of Successful Selling e-Book</div>
+                                <div class="cardDate h4 text-white fw-semi-bold">15.08.19</div>
+                            </div>
+                        </div>
+                    </a>
+                    
+                </div>
+
+            </div>
+
+
+        </div>
+    </div>
+</section>
