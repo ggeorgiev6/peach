5798892cc0341d31b38c1e646c055f8b67d4ebee
ggeorgiev6 <gecatalost@gmail.com>
2022-05-27 20:41:18 +0100

    Footer

--- resources/views/partials/global/foot.blade.php
+++ resources/views/partials/global/foot.blade.php
@@ -1,2 +1,82 @@
 <footer>
+    <div class="footerWrap bg-secondary">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-12 col-lg-2">
+                    <img class="footerLogo" src="/images/elation-brandmark-logo.svg" alt="">
+                </div>
+
+                <div class="col-12 col-lg-2">
+                   <div class="footerNavListWrap">
+                       <div class="footerNavListTitle text-primary fw-semi-bold">Products</div>
+                       <ul class="footerNavList">
+                            <li class="h4">
+                                <a class="text-white" href="/">Growth Framework</a>
+                            </li>
+                            <li class="h4">
+                                <a class="text-white" href="/">Growth Academy</a>
+                            </li>
+                            <li class="h4">
+                                <a class="text-white" href="/">Growth Club</a>
+                            </li>
+                       </ul>
+                   </div>
+                </div>
+
+                <div class="col-12 col-lg-2">
+                   <div class="footerNavListWrap">
+                       <div class="footerNavListTitle text-primary fw-semi-bold">Company</div>
+                       <ul class="footerNavList">
+                            <li class="h4">
+                                <a class="text-white" href="/">About</a>
+                            </li>
+                            <li class="h4">
+                                <a class="text-white" href="/">News & Insights</a>
+                            </li>
+                            <li class="h4">
+                                <a class="text-white" href="/">Growth Club</a>
+                            </li>
+                       </ul>
+                   </div>
+                </div>
+
+                <div class="col-12 col-lg-2">
+                   <div class="footerNavListWrap">
+                       <div class="footerNavListTitle text-primary fw-semi-bold">Help</div>
+                       <ul class="footerNavList">
+                            <li class="h4">
+                                <a class="text-white" href="/">FAQ</a>
+                            </li>
+                            <li class="h4">
+                                <a class="text-white" href="/">Contact</a>
+                            </li>
+                            <li class="h4">
+                                <a class="text-white" href="/">Resources</a>
+                            </li>
+                       </ul>
+                   </div>
+                </div>
+
+                <div class="col-12 col-lg-3 offset-lg-1">
+                    <ul class="socialMediaLinks">
+                        <li class="socialMediaIcon">
+                            <a class="" href="">
+                                <div class="socialMEdiaIconShape">
+                                    {!! file_get_contents('images/icons/sm-shape.svg') !!}
+                                </div>
+                                <div class="socialMediaIconElem">
+                                    {!! file_get_contents('images/icons/fb-icon.svg') !!}
+                                </div>
+                               
+                            </a>
+                        </li>
+                       
+                    </ul>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
 </footer>
